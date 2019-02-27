0ee0bd67d0e25ecbc2ae09181c01ea4ab20e9439
Richard Crooks <email@rocrooks.co.uk>
2019-02-27 12:48:50 +0000

    Evidence database model created

--- var_db/models.py
+++ var_db/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Evidence(models.Model):
+    VariantID = models.IntegerField()
+    EvidenceCode = models.CharField(max_length=4)
+    DateAssigned = model.DateTimeField(default = timezone.now)
+    DateRetired = model.DateTimeField(blank = True, null = True)
