c5c537623fbc28880b0e19cf3196304a7dad20b4
christine <christinehicks@hotmail.co.uk>
2019-02-27 12:49:49 +0000

    added models for patient, variants and patient_variant

--- var_db/models.py
+++ var_db/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    proband = models.BooleanField()
+    affected = models.BooleanField()
+    relatives = models.BooleanField()
+    stage = models.IntegerField()
+    description = models.CharField(max_length=20)
+    patient_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return (self.patient_id)
+
+
+class Variant(models.Model):
+    sequencer = models.CharField(max_length=15)
+    variant_cnda = models.CharField(max_length = 50)
+    variant_protein = models.CharField(max_length=50)
+    variant_genome = models.CharField(max_length=50)
+    variant_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return (self.variant_id)
+
+class Patient_Variant(models.Model):
+    patient = models.IntegerField()
+    variant = models.IntegerField()
+    date_created = models.DateTimeField(default = timezone.now)
+    classification = models.IntegerField()
