1efd5f0eaeceee4e75306e9239b76082a4b5abea
Richard Crooks <email@rocrooks.co.uk>
2019-02-28 11:26:01 +0000

    Updating models to reflect amended database structure

--- var_db/models.py
+++ var_db/models.py
@@ -1,36 +1,42 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Patient(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    proband = models.BooleanField()
-    affected = models.BooleanField()
-    relatives = models.BooleanField()
-    stage = models.IntegerField()
-    description = models.CharField(max_length=20)
-    patient_id = models.AutoField(primary_key=True)
-    created_date = models.DateTimeField(default=timezone.now)
+    PatientID = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=100)
+    Surname = models.CharField(max_length=100)
+    Age = models.IntegerField()
+    Proband = models.CharField()
+    Relatives = models.CharField()
+    Stage = models.IntegerField()
+    Description = models.CharField(max_length=20)
+
     def __str__(self):
-        return (self.patient_id)
+
+        return self.patient_id
+
 
 class Variant(models.Model):
-    sequencer = models.CharField(max_length=15)
-    variant_cnda = models.CharField(max_length = 50)
-    variant_protein = models.CharField(max_length=50)
-    variant_genome = models.CharField(max_length=50)
-    variant_id = models.AutoField(primary_key=True)
-    created_date = models.DateTimeField(default=timezone.now)
+    VariantID = models.AutoField(primary_key=True)
+    cDNA = models.CharField(max_length = 50)
+    Protein = models.CharField(max_length=50)
+    Genome = models.CharField(max_length=50)
+
     def __str__(self):
+
         return (self.variant_id)
 
 class Patient_Variant(models.Model):
-    patient = models.IntegerField()
-    variant = models.IntegerField()
-    date_created = models.DateTimeField(default=timezone.now)
-    classification = models.IntegerField()
+    ObservationID = models.AutoField(primary_key=True)
+    PatientID = models.IntegerField()
+    VariantID = models.IntegerField()
+    Sequencer = models.CharField(max_length=50)
+    Pathogenicity = models.IntegerField()
+    DateObserved = models.DateTimeField(default=timezone.now)
 
 class Evidence(models.Model):
+    EvidenceID = models.AutoField(primary_key=True)
     VariantID = models.IntegerField()
     EvidenceCode = models.CharField(max_length=4)
     DateAssigned = models.DateTimeField(default=timezone.now)
