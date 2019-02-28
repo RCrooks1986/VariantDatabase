24efab21d306b4d128595efd71c2dde47b47a204
christine <christinehicks@hotmail.co.uk>
2019-02-28 16:12:16 +0000

    file uploads

--- var_db/views.py
+++ var_db/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .forms import VariantForm, ImportCSV
-from .models import Variant
+from .models import VariantData
 import csv
 
 # Create your views here.
@@ -20,7 +20,7 @@ def search_variant(request):
             input_genomic = form.cleaned_data['genomic_position']
             print(input_gene)
 
-            variant_data = Variant.objects.all() #filter(field_in_db_filtering_on = input_gene)
+            variant_data = VariantData.objects.all() #filter(field_in_db_filtering_on = input_gene)
             # pass this within context to the rendered html
             # if multiple records, iterate over within template
 
@@ -44,23 +44,37 @@ def upload_file(request):
             file_contents = request.FILES['upfile'].read().decode('UTF-8')
             lines = file_contents.split('\n')
             for line in lines:
-                line = line.rstrip()
-                field = line.split('\t')
-                print("##", field[0])
+                if line.startswith("Name"):
+                    pass
+                else:
+                    line = line.rstrip()
+                    field = line.split('\t')
 
-                first_name = field[0].split(' ')[0]
-                surname = field[0].split(' ')[1]
-                age = field[1]
-                proband = field[2]
-                affected_relatives = field[3]
-                stage = field[4]
-                description = field[5]
-                sequencer = field[6]
-                cnda = field[7]
-                protein = field[8]
-                genome = field[9]
-                pathogenicity = field[11]
-                evidence_codes = field[12]
+                    first_name = field[0].split(' ')[0]
+                    surname = field[0].split(' ')[1]
+                    age = field[1]
+                    proband = field[2]
+                    affected_relatives = field[3]
+                    stage = field[4]
+                    description = field[5]
+                    sequencer = field[6]
+                    cdna = field[7]
+                    protein = field[8]
+                    genome = field[9]
+                    pathogenicity = field[11]
+                    if len(field) == 13:
+                        evidence_codes = field[12]
+                    else:
+                        evidence_codes = 'NA'
+                    print(evidence_codes)
+
+                    data = VariantData(first_name=first_name, surname=surname, age=age, proband=proband,
+                                       affected_relatives=affected_relatives, stage=stage, description=description,
+                                       sequencer=sequencer, cnda=cdna, protein=protein,genome=genome,
+                                       pathogenicity=pathogenicity, evidence_codes=evidence_codes )
+                    data.save()
+
+
 
         return HttpResponse("File upload page")
     if request.method=="GET":
